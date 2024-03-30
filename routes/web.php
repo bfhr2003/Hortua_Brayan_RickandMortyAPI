6df6a797168f56c0f196e568d26c323cfe0e4bf9
bfhr2003 <b_hortuarodrig152861@fanshaweonline.ca>
2024-03-30 17:35:17 -0400

    Creating my own controllers and models

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,11 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/books', 'BookController@getAll');
-$router->get('/books/{id}', 'BookController@getOne');
-$router->post('/books/add', 'BookController@save');
-$router->post('/books/edit/{id}', 'BookController@update');
-$router->delete('/books/delete/{id}', 'BookController@delete');
+$router->get('/characters', 'CharacterController@getAll');
+$router->get('/characters/{id}', 'CharacterController@getOne');
+$router->post('/characters/add', 'CharacterController@save');
+$router->post('/characters/edit/{id}', 'CharacterController@update');
+$router->delete('/characters/delete/{id}', 'CharacterController@delete');
 
 
 
